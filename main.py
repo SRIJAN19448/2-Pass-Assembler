6b24b345cc9cbf3da323bba29a6fc9a917c01d70
Vaibhav Soni <55681265+vaibhav2808@users.noreply.github.com>
2020-03-07 14:25:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import re
 opcode_dict=dict()
 symbol_table=dict()     #format [name,type_variable/label,location_counter_address]    not finalised
+literal_table=dict()
 
 def opcode_initialise():                       #need to change ones to zero wherever necessary to account for no operand opcodes
     global opcode_dict
-    opcode_dict['CLA']=['0000',1]
+    opcode_dict['CLA']=['0000',0]
     opcode_dict['LAC']=['0001',1]
     opcode_dict['SAC']=['0010',1]
     opcode_dict['ADD']=['0011',1]
     opcode_dict['SUB']=['0100',1]
-    opcode_dict['BRZ']=['0101',1]
-    opcode_dict['BRN']=['0110',1]
-    opcode_dict['BRP']=['0111',1]
+    opcode_dict['BRZ']=['0101',2]
+    opcode_dict['BRN']=['0110',2]
+    opcode_dict['BRP']=['0111',2]
     opcode_dict['INP']=['1000',1]
     opcode_dict['DSP']=['1001',1]
     opcode_dict['MUL']=['1010',1]
     opcode_dict['DIV']=['1011',1]
-    opcode_dict['STP']=['1100',1]
+    opcode_dict['STP']=['1100',0]
 
 class Pass_One:
     global opcode_dict
+    global literal_table
+    global symbol_table
     location_counter=0
     line_number=0           #later use for error handling
     error_found=False                                    #used to know if error is found so no second pass required
@@ -91,25 +94,24 @@ class Pass_One:
             opcode=''
             label=''
             length=0                               #maybe required not sure
+            
             if self.is_end_statement(line):
                 break
-                
+            
             if self.is_a_comment(line):             #checks for comment
                 continue
-            comm=line.find('//')
+            
+            comm=line.find('//')                    #for comments after the assembly line
             if comm!=-1:
                 line=line[:comm]
             line=line.strip()
             line=re.findall('\S+',line)
-            
-            if(len(line)==0):                                    #for empty lines :/
+
+            if(len(line)==0):                     #for empty lines :/
                 continue
 
             if(len(line)>3):                       #error for more operands
                 pass
-            
-            file_temp.write(' '.join(line))                 #write to the temporary file
-            file_temp.write('\n')
 
             if self.is_declarative_statement(line):       #make function for handling declarations here
                 continue
@@ -117,7 +119,10 @@ class Pass_One:
             
             if self.has_a_label(line):
                 label=self.extract_label(line)  #send it to symbol table later and check if it already exists for error reporting 
-                line=line[1:]  
+                line=line[1:]
+
+            file_temp.write(' '.join(line))                 #write to the temporary file
+            file_temp.write('\n')  
             
             opcode=self.extract_opcode(line)
             if self.valid_opcode(opcode):   #requires case for invalid for now
@@ -134,11 +139,45 @@ class Pass_One:
         file_assemble.close()
         file_error.close()
         file_symbol_table.close()
-   
+
+class Pass_two:
+    global symbol_table
+    global opcode_dict
+    global literal_table
+    
+    def __init__(self):
+        self.filename='temp.txt'
+    
+    def main(self):
+        file_open=open(self.filename,'r')
+        file_output=open('output.txt','w')
+
+        for line in file_open:
+            line=line.rstrip()
+            line=line.split(' ')
+            opcode=line[0]
+            type=opcode_dict[opcode][1]
+            file_output.write(opcode_dict[opcode][0])
+            if type!=0:
+                var=line[1]
+                try:
+                    binary=bin(symbol_table[var][0]).replace("0b",'')
+                except:
+                    binary=bin(literal_table[var][0]).replace("0b",'')
+                l=8-len(binary)
+                binary='0'*l+binary
+                file_output.write(' ')
+                file_output.write(binary)
+            file_output.write('\n')
+
+
+
+
 def pass_one():
     filen=input("Enter the filename ")
     obj=Pass_One(filen)
     obj.main()
 
 opcode_initialise()
-pass_one()
+
+
