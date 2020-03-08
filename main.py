bafe07ca151a2cc0aeac45ce52750228e608ae7e
SRIJAN19448 <55681238+SRIJAN19448@users.noreply.github.com>
2020-03-08 08:13:28 +0530

    pass 1 fully functional and complete
    check it using the given test.txt file

--- main.py
+++ main.py
@@ -1,144 +1,194 @@
 import re
-opcode_dict=dict()
-symbol_table=dict()     #format [name,type_variable/label,location_counter_address]    not finalised
+error=False
+program_counter=0
+op_table = dict()
+symbol_table = []  # format [name,type_variable/label,location_counter_address]    not finalised
+literal_table=[]
+error_list=[]
 
-def opcode_initialise():                       #need to change ones to zero wherever necessary to account for no operand opcodes
-    global opcode_dict
-    opcode_dict['CLA']=['0000',1]
-    opcode_dict['LAC']=['0001',1]
-    opcode_dict['SAC']=['0010',1]
-    opcode_dict['ADD']=['0011',1]
-    opcode_dict['SUB']=['0100',1]
-    opcode_dict['BRZ']=['0101',1]
-    opcode_dict['BRN']=['0110',1]
-    opcode_dict['BRP']=['0111',1]
-    opcode_dict['INP']=['1000',1]
-    opcode_dict['DSP']=['1001',1]
-    opcode_dict['MUL']=['1010',1]
-    opcode_dict['DIV']=['1011',1]
-    opcode_dict['STP']=['1100',1]
+def opcode_initialise():  # need to change ones to zero wherever necessary to account for no operand opcodes
+    op_table['CLA'] = ['0000']
+    op_table['LAC'] = ['0001']
+    op_table['SAC'] = ['0010']
+    op_table['ADD'] = ['0011']
+    op_table['SUB'] = ['0100']
+    op_table['BRZ'] = ['0101']
+    op_table['BRN'] = ['0110']
+    op_table['BRP'] = ['0111']
+    op_table['INP'] = ['1000']
+    op_table['DSP'] = ['1001']
+    op_table['MUL'] = ['1010']
+    op_table['DIV'] = ['1011']
+    op_table['STP'] = ['1100']
 
-class Pass_One:
-    global opcode_dict
-    location_counter=0
-    line_number=0           #later use for error handling
-    error_found=False                                    #used to know if error is found so no second pass required
-    
-    def __init__(self,file):
-        self.filename=file
-        pass
-    
-    def is_a_comment(self,str):
-        return str.startswith("//")
-    
-    def has_a_label(self,str):
-        a=str[0].find(':')
-        if a!=-1:
-            return True
-        return False
-    
-    def extract_label(self,str):
-        return str[0]
+def passOne(text):
+    global program_counter
+    global error
+    global error_list
+    STP=False                       #STP command found or not
 
-    def valid_opcode(self,str):
-        a=opcode_dict.get(str,-2)
-        if a==-2:
-            return False
-        return True
+    for a in text:
+        if a=='' or a.startswith("//"):
+            text.remove(a)
 
-    def extract_opcode(self,str):
-        return str[0]
- 
-    def extract_variable(self,str): 
-        return str[1]
-
-    def is_assembler_directive(self,str):
-        pass
-
-    def has_literal(self,str):
-        pass
-
-    def is_end_statement(self,str):
-        if str.startswith("END"):
-            return True
-        return False
-
-    def is_declarative_statement(self,str):
-        pass
-
-    def is_a_stop(self,str):                  #used to detect stop
-        pass
-
-    def is_start(self,str):                         #used to detect start of the assembly program
-        pass
-
-    def check_type_of_instruction(self,str):           #check whether needs a variable or not
-        pass
-
-    def main(self):
-        self.location_counter=0
-        line_number=1
-        file_assemble=open(self.filename,'r')
-        file_temp=open("temp.txt",'w')
-        file_error=open("error.txt",'w')
-        file_symbol_table=open("symbol_table.txt",'w')
-        file_literal_table=open("literal_table.txt",'w')
-
-       
-        for line in file_assemble:
-            line=line.strip()
-            operand=''
-            opcode=''
-            label=''
-            length=0                               #maybe required not sure
-            if self.is_end_statement(line):
+    for b in text:
+        b=b.split(" ")
+        for s in range(len(b)):
+            if b[s].startswith("//"):
+                b=b[:s]
                 break
-                
-            if self.is_a_comment(line):             #checks for comment
-                continue
-            comm=line.find('//')
-            if comm!=-1:
-                line=line[:comm]
-            line=line.strip()
-            line=re.findall('\S+',line)
-            
-            if(len(line)==0):                                    #for empty lines :/
-                continue
 
-            if(len(line)>3):                       #error for more operands
-                pass
-            
-            file_temp.write(' '.join(line))                 #write to the temporary file
-            file_temp.write('\n')
+        if len(b)==1:
+            if b[0]=="STP":
+                STP=True
+            elif b[0]!="CLA":
+                error=True
+                error_list.append("LESS ARGUMENTS THEN REQUIRED : " + str(b[0]))
+        elif len(b)==2:
+            if b[0][-1]==":":
+                symb=b[0][:len(b[0])-1]
+                flag=False
+                for sym in symbol_table:
+                    if sym['name']==symb:
+                        if sym['found']==True:
+                            error=True
+                            error_list.append("MULTIPLE LABEL DECLARATION : " + sym['name'])
+                        else:
+                            sym['found']=True
+                            sym['addr']=program_counter
+                            flag=True
+                if flag==False:
+                    symbol_table.append({'name':symb ,'type':"label",'found':True,'used':False,'addr':program_counter})
+                    
+                if b[1] == "STP":
+                    STP = True
+                elif b[1] != "CLA":
+                    error = True
+                    error_list.append("INVALID OPCODE : " + b[1])
+            elif b[0][0]=="B":
+                if b[0]=="BRZ"or b[0]=="BRN" or b[0]=="BRP":
+                    symb=b[1]
+                    flag=False
+                    for sym in symbol_table:
+                        if sym['name']==symb:
+                            sym['used']=True
+                            flag=True
+                    if flag==False:
+                        symbol_table.append({'name':symb,'type':"label",'found':False,'used':True,'addr':-1})
 
-            if self.is_declarative_statement(line):       #make function for handling declarations here
-                continue
-                
-            
-            if self.has_a_label(line):
-                label=self.extract_label(line)  #send it to symbol table later and check if it already exists for error reporting 
-                line=line[1:]  
-            
-            opcode=self.extract_opcode(line)
-            if self.valid_opcode(opcode):   #requires case for invalid for now
-                a=opcode_dict.get(opcode)
-                if a[1]==1:                        #nothing done for literal yet
-                    operand=self.extract_variable(line)
-            else:                                       #write code for error reporting for invalid opcode
-                pass
-            
-            line_number+=1
-            self.location_counter+=1
-            
-        file_temp.close()
-        file_assemble.close()
-        file_error.close()
-        file_symbol_table.close()
-   
-def pass_one():
-    filen=input("Enter the filename ")
-    obj=Pass_One(filen)
-    obj.main()
+                else:
+                    error=True
+                    error_list.append("INVALID OPCODE : " + b[0])
+            else:
+                if b[0] in op_table:
+                    flag=False
+                    if b[1][0]!="=":
+                        for sym in symbol_table:
+                            if sym['name'] == b[1]:
+                                flag=True
+
+                        if flag==False:
+                            symbol_table.append({'name': b[1], 'type': "symbol",'addr': -1})
+                    else:
+                        flag=False
+                        for lit in literal_table:
+                            if lit['literal']==b[1]:
+                                flag=True
+
+                        if flag==False:
+                            literal_table.append({'literal':b[1], 'addr':-1})
+
+                else:
+                    error=True
+                    error_list.append("INVALID OPCODE")
+
+        elif len(b)==3:
+            if b[0] in op_table:
+                error=True
+                error_list.append("EXTRA ARGUMENTS THAN REQUIRED : " + b[0])
+            elif b[0][-1]!=":":
+                error=True
+                error_list.append("INVALID COMMAND")
+            else:
+                symb = b[0][:len(b[0]) - 1]
+                flag = False
+                for sym in symbol_table:
+                    if sym['name'] == symb:
+                        if sym['found']==True:
+                            error=True
+                            error_list.append("MULTIPLE LABEL DECLARATION : " + sym['name'])
+                        else:
+                            sym['found'] = True
+                            sym['addr'] = program_counter
+                            flag = True
+                if flag == False:
+                    symbol_table.append({'name': symb, 'type': "label", 'found': True, 'used': False, 'addr': program_counter})
+
+                if b[1] in op_table:
+                    flag = False
+                    if b[2][0]!="=":
+                        for sym in symbol_table:
+                            if sym['name'] == b[2]:
+                                flag = True
+                                continue
+                        if flag == False:
+                            symbol_table.append({'name': b[2], 'type': "symbol", 'found': False, 'used': True, 'addr': -1})
+                    else:
+                        flag = False
+                        for lit in literal_table:
+                            if lit['literal'] == b[2]:
+                                flag = True
+
+                        if flag == False:
+                            literal_table.append({'literal': b[2], 'addr': -1})
+
+                else:
+                    error = True
+                    error_list.append("INVALID OPCODE : " + b[1])
+        program_counter+=1
+
+    for y in symbol_table:
+        if y['type']=="label":
+            if y['found']==True and y['used']==False:
+                error=True
+                error_list.append("LABEL DEFINED BUT NOT USED")
+            elif y['found']==False and y['used']==True:
+                error=True
+                error_list.append("LABEL NOT DEFINED")
+    for y in symbol_table:
+        try :
+            c= int(y['name'])
+            y['addr'] = c
+            if c>=256:
+                error=True
+                error_list.append("ADDRESS OUT OF MEMORY: " + y['name'])
+        except:
+            pass
+    for y in symbol_table:
+        if y['type']=="symbol" and y['addr']==-1:
+            y['addr']=program_counter
+            program_counter+=1
+    for y in literal_table:
+        if y['addr']==-1:
+            y['addr']=program_counter
+            program_counter+=1
+
 
 opcode_initialise()
-pass_one()
+     
+while True:
+    fileaddr = input("Input file address")
+    try:
+        file = open(fileaddr, 'r')
+        break
+    except :
+        print("No File Found, Kindly Retry")
+
+
+text = file.read()                              # read from the file
+text = text.split('\n')
+print(text)
+passOne(text)
+print(symbol_table)
+print(literal_table)
+print(error_list)
